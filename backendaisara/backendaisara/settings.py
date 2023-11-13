c8738cf02821fe9cb994bc673383e28e21e275b6
aisara21 <119093375+aisara21@users.noreply.github.com>
2023-11-13 22:05:02 +0600

    Update settings.py

--- backendaisara/backendaisara/settings.py
+++ backendaisara/backendaisara/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_bdgi3zui@*rm0xei&o7m%8@%2*^7b)n9qhzrd&mjlkrnx6r1l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.232.130.142']
 
 
 # Application definition
